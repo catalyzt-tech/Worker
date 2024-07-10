e6dae03cfe99931106efef4e4b04eb42ebe6f359
Billy19191 <bill.ratchanon@gmail.com>
2024-07-11 00:00:13 +0700

    fix: retroPGF4 project license status for ineligible projects

--- src/scripts/rpgf-4-merge-license-status.ts
+++ src/scripts/rpgf-4-merge-license-status.ts
@@ -2,20 +2,26 @@ import rpgf4_license_status from '../../data/RetroPGF4/optimismResource/rpgf4_li
 import rpgf4Data from '../../data/RetroPGF4/(6)rpgf4_merged_impact_metric.json'
 import fs from 'fs'
 const run = () => {
+  let count = 0
   const rpgf4DataWithLicense = rpgf4Data.map((data) => {
     const license = rpgf4_license_status.find(
       (license) => license.ProjectID === data.projectId
     )
     if (license) {
+      count++
       const newData = {
         ...data,
         isOpenSource: license['OS Status'] === 'Approved' ? true : false,
       }
       return newData
     }
-    return data
+    const newData = {
+      ...data,
+      isOpenSource: null,
+    }
+    return newData
   })
-  console.log(JSON.stringify(rpgf4DataWithLicense))
+  console.log('Matched:', count)
   fs.writeFileSync(
     './data/RetroPGF4/(7)rpgf4_merged_with_license_source.json',
     JSON.stringify(rpgf4DataWithLicense)
