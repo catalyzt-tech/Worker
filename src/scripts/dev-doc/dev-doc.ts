b32267fbc890deeb7698f0c72263a1476b992d2f
tgrziminiar <mixzazazaza1212@gmail.com>
2024-08-26 01:03:44 +0700

    fix: change cronTimer to CRON_TIMER & change base DATA_DIR

--- src/scripts/dev-doc/dev-doc.ts
+++ src/scripts/dev-doc/dev-doc.ts
@@ -2,10 +2,10 @@ import { ALL_DOCS_DEV_PATH, BASE_DOC_DEV } from "../../../const";
 import { ListAllFilesInGithub } from "../../../lib/list-all-files-github";
 import { SaveFileMdxGithub } from "../../../lib/op-github/save-file-mdx-from-github";
 
-const DATA_DIR = ['..', '..', 'data', 'dev-doc-data']
+const DATA_DIR = ['data', 'dev-doc-data']
 
 // Which evaluates to 'At 0 seconds, 0 minutes every 1st hour'.
-const cronTimer:string | undefined = undefined
+const CRON_TIMER:string | undefined = undefined
 
 async function Run() {
     console.log("Dev Docs is starting . . .");
@@ -24,4 +24,4 @@ async function Run() {
     }
 }
 
-export {Run, DATA_DIR, cronTimer}
+export {Run, DATA_DIR, CRON_TIMER}
